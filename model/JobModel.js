2b5b4c2f5084655b10b926b0c75aa406358223b8
Yunus Sen <46409298+younussenn@users.noreply.github.com>
2019-12-27 00:57:24 +0300

    Update JobModel.js

--- model/JobModel.js
+++ model/JobModel.js
@@ -1,8 +1,11 @@
 class JobModel{
     constructor(id, ownerId,bgColor, description){
         this.id = id;
+        
         this.ownerId= ownerId;
+        
         this.bgColor = bgColor;
+        
         this.description = description;
     }
 }
